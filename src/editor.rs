7c2dcf6a38ca1ca5cf1dbc807c47c89b19abe5de
ddd-cmbck <dd.darknet.tt@gmail.com>
2024-10-15 19:38:55 +0100

    improved code structure

--- src/editor.rs
+++ src/editor.rs
@@ -1,11 +1,9 @@
-use std::io::stdout;
 use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
-use crossterm::queue;
-use crossterm::style::Print;
+use std::io::Error;
 
 mod terminal;
 
-use terminal::Terminal;
+use terminal::{Terminal, Size, Position};
 
 pub struct Editor{
     should_quit: bool,
@@ -23,7 +21,7 @@ impl Editor {
         result.unwrap();
     }
 
-    fn repl(&mut self) -> Result<(), std::io::Error> {
+    fn repl(&mut self) -> Result<(), Error> {
         loop {
             self.refresh_screen()?;
             if self.should_quit {
@@ -49,25 +47,26 @@ impl Editor {
         }
     }
 
-    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+    fn refresh_screen(&self) -> Result<(), Error> {
+        Terminal::hide_cursor()?;
         if self.should_quit {
-            Terminal::hide_cursor()?;
             Terminal::clear_screen()?;
-            print!("Goodbye.\r\n");
+            Terminal::print("Goodbye!\r\n")?;
         } else {
-            Terminal::hide_cursor()?;
             Self::draw_rows()?;
-            Terminal::move_cursor_to(0, 0)?;
-            Terminal::show_cursor()?;
+            Terminal::move_cursor_to(Position {x: 0, y: 0})?;
         }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
         Ok(())
     }
 
-    fn draw_rows() -> Result<(), std::io::Error> {
-        let height = Terminal::size()?.1;
+    fn draw_rows() -> Result<(), Error> {
+        let Size {height, .. } = Terminal::size()?;
         for current_row in 0 .. height {
-            queue!(stdout(), Print("~"))?;
-            if current_row + 1 < height { queue!(stdout(), Print("\r\n"))?; }
+            Terminal::clear_line()?;
+            Terminal::print("~")?;
+            if current_row + 1 < height { Terminal::print("\r\n")?; }
         }
         Ok(())
     }
